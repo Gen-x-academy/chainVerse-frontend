02db1ff1805c48adb01a52179964933daf9fda29
shamoo53 <aliyushamsu16@gmail.com>
2025-03-22 15:02:55 +0100

    Implementation of Featured Courses Section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,14 @@
 import NavBar from "@/components/ui/NavBar";
+import FeaturedCourses from "@/components/ui/FeaturedCourses";
 
 export default function Home() {
   return (
-   <div className=" font-extrabold text-4xl text-center">
-    <NavBar />
-    chainVerse Academy
- 
-   </div>
+    <div>
+      <NavBar />
+      <div className="font-extrabold text-4xl text-center py-10">
+        chainVerse Academy
+      </div>
+      <FeaturedCourses />
+    </div>
   );
 }
