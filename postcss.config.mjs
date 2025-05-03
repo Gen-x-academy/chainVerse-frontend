0d59b0cd7fadab5b9249fcb9993bae5eb4ad5cb6
Abdulmalik A. <amalikabdulmalik04@gmail.com>
2025-05-03 15:48:31 +0100

    resolve conflict

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,5 @@
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-
 const config = {
-  plugins: {
-    "@tailwindcss/postcss": [tailwindcss, autoprefixer],
-  },
+  plugins: ["@tailwindcss/postcss"],
 };
 
 export default config;
